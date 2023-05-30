f0d685ba53330bdc8b6d696c9a601a3d3644bb7c
Merhandes <126306327+Merhandes@users.noreply.github.com>
2023-05-30 17:01:13 +0700

    Initial Commit

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,16 +1,16 @@
-import {useState, useEffect} from "react";
-import {Navbar, Container, Nav} from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
-import { navLinks } from '../data/index';
-import { NavLink } from 'react-router-dom';
+import { navLinks } from "../data/index";
+import { NavLink } from "react-router-dom";
 
 const NavbarComponent = () => {
   const [changeColor, setChangeColor] = useState(false);
 
   const changeBackgroundColor = () => {
-    if(window.scrollY > 10){
+    if (window.scrollY > 10) {
       setChangeColor(true);
-    } else{
+    } else {
       setChangeColor(false);
     }
   };
@@ -24,31 +24,34 @@ const NavbarComponent = () => {
   return (
     <div>
       <Navbar expand="lg" className={changeColor ? "color-active" : ""}>
-      <Container>
-        <Navbar.Brand className="fs-3 fw-bold"href="#home">Patriot.</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto text-center">
-            {navLinks.map((link) => {
-              return (
-              <div className='nav-link' key={link.id}>
-                <NavLink to={link.path} className={({ isActive, isPending }) =>
-                           isPending ? "pending" : isActive ? "active" : ""} end>
-                {link.text}
-                </NavLink>
-              </div>
-              );
-            })}
-          </Nav>
-
-          <div className="text-center">
-            <button className="btn btn-outline-danger rounded-1">About Us</button>
-          </div>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <Container>
+          <Navbar.Brand className="fs-3 fw-bold" href="#home">
+            Patriot.
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto text-center">
+              {navLinks.map((link) => {
+                return (
+                  <div className="nav-link" key={link.id}>
+                    <NavLink
+                      to={link.path}
+                      className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : ""
+                      }
+                      end
+                    >
+                      {link.text}
+                    </NavLink>
+                  </div>
+                );
+              })}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default NavbarComponent
+export default NavbarComponent;
--- src/data/index.js
+++ src/data/index.js
@@ -66,7 +66,8 @@ export const semuaPahlawan = [
     descripion:
       "Pemimpin Batak yang melakukan kampanye gerilyawan melawan pasukan kolonial Belanda.",
     button: "Lihat Selengkapnya",
-    delay: "200"
+    delay: "200",
+    url: "https://id.wikipedia.org/wiki/Sisingamangaraja_XII"
   },
   {
     id: 3,
@@ -76,7 +77,8 @@ export const semuaPahlawan = [
     descripion:
       "Dijuluki Ayam Jantan dari Timur oleh Belanda, beliau melakukan perlawanan terhadap pasukan kolonial Belanda.",
     button: "Lihat Selengkapnya",
-    delay: "400"
+    delay: "400",
+    url: "https://id.wikipedia.org/wiki/Sultan_Hasanuddin"
   },
   {
     id: 4,
@@ -86,7 +88,8 @@ export const semuaPahlawan = [
     descripion:
       "Pemimpin Bali yang melakukan perlawanan terhadap pasukan kolonial Belanda.",
     button: "Lihat Selengkapnya",
-    delay: ""
+    delay: "",
+    url: "https://id.wikipedia.org/wiki/I_Gusti_Ketut_Jelantik"
   },
   {
     id: 5,
@@ -96,7 +99,8 @@ export const semuaPahlawan = [
     descripion:
       "Ia adalah pahlawan yang berjuang untuk Maluku melawan VOC Belanda, mantan sersan di militer Inggris.",
     button: "Lihat Selengkapnya",
-    delay: ""
+    delay: "",
+    url: "https://id.wikipedia.org/wiki/Pattimura"
   },
   {
     id: 6,
@@ -106,7 +110,8 @@ export const semuaPahlawan = [
     descripion:
       "Pemimpin Islam yang melakukan perlawanan terhadap pasukan pendudukan Jepang.",
     button: "Lihat Selengkapnya",
-    delay: ""
+    delay: "",
+    url: "https://id.wikipedia.org/wiki/Zainal_Mustafa"
   },
   {
     id: 7,
@@ -116,7 +121,8 @@ export const semuaPahlawan = [
     descripion:
       "Selama masa pendudukan Jepang di Indonesia, ia menjadi pemimpin pemberontakan milisi PETA di Blitar.",
     button: "Lihat Selengkapnya",
-    delay: ""
+    delay: "",
+    url: "https://id.wikipedia.org/wiki/Soeprijadi"
   },
   {
     id: 8,
@@ -126,7 +132,8 @@ export const semuaPahlawan = [
     descripion:
       "Pemimpin militer yang memimpin perlawanan dalam Pertempuran Surabaya.",
     button: "Lihat Selengkapnya",
-    delay: ""
+    delay: "",
+    url: "https://id.wikipedia.org/wiki/Sutomo"
   },
   {
     id: 9,
@@ -136,7 +143,8 @@ export const semuaPahlawan = [
     descripion:
       "Komandan Ketua Tentara Nasional Indonesia pada saat Revolusi Nasional, sosok yang dihormati di Indonesia.",
     button: "Lihat Selengkapnya",
-    delay: ""
+    delay: "",
+    url: "https://id.wikipedia.org/wiki/Soedirman"
   },
   {
     id: 10,
@@ -146,7 +154,8 @@ export const semuaPahlawan = [
     descripion:
       "Memimpin militer Bali saat Revolusi Nasional langsung perlawanan bersenjata anti-Belanda di Bali. ",
     button: "Lihat Selengkapnya",
-    delay: ""
+    delay: "",
+    url: "https://id.wikipedia.org/wiki/I_Gusti_Ngurah_Rai"
   },
   {
     id: 11,
@@ -156,7 +165,8 @@ export const semuaPahlawan = [
     descripion:
       "Pejuang kemerdekaan menentang pemerintah Hindia Belanda di Tanah Karo.",
     button: "Lihat Selengkapnya",
-    delay: ""
+    delay: "",
+    url: "https://id.wikipedia.org/wiki/Jamin_Ginting"
   },
   {
     id: 12,
@@ -166,7 +176,30 @@ export const semuaPahlawan = [
     descripion:
       "Komodor Angkatan Laut; terbunuh saat konfrontasi dengan Belanda di Nugini Belanda.",
     button: "Lihat Selengkapnya",
-    delay: ""
+    delay: "",
+    url: "https://id.wikipedia.org/wiki/Yos_Sudarso"
+  },
+  {
+    id: 13,
+    image: PahlawanImage13,
+    title: "Teuku Abdul Hamid Azwar",
+    wilayah: "(Aceh)",
+    descripion:
+      "Memimpin gerilyawan Aceh dalam serangan dan sabotase terhadap pasukan pendudukan Jepang.",
+    button: "Lihat Selengkapnya",
+    delay: "1900",
+    url: "https://id.wikipedia.org/wiki/Teuku_Abdul_Hamid_Azwar"
+  },
+  {
+    id: 14,
+    image: PahlawanImage14,
+    title: "Haji Madriyan",
+    wilayah: "(Indramayu, Jawa)",
+    descripion:
+      "Memimpin rakyat Indramayu dalam melawan kekejaman pasukan pendudukan Jepang.",
+    button: "Lihat Selengkapnya",
+    delay: "",
+    url: "https://ipsgampang.blogspot.com/2023/04/haji-madriyan-h-madriyas.html"
   }
 ];
 
@@ -190,7 +223,8 @@ export const PahlawanBelanda = [
     descripion:
       "Pemimpin Batak yang melakukan kampanye gerilyawan melawan pasukan kolonial Belanda.",
     button: "Lihat Selengkapnya",
-    delay: "1600"
+    delay: "1600",
+    url: "https://id.wikipedia.org/wiki/Sisingamangaraja_XII"
   },
   {
     id: 3,
@@ -200,7 +234,8 @@ export const PahlawanBelanda = [
     descripion:
       "Dijuluki Ayam Jantan dari Timur oleh Belanda, beliau melakukan perlawanan terhadap pasukan kolonial Belanda.",
     button: "Lihat Selengkapnya",
-    delay: "1900"
+    delay: "1900",
+    url: "https://id.wikipedia.org/wiki/Sultan_Hasanuddin"
   },
   {
     id: 4,
@@ -210,7 +245,8 @@ export const PahlawanBelanda = [
     descripion:
       "Pemimpin Bali yang melakukan perlawanan terhadap pasukan kolonial Belanda.",
     button: "Lihat Selengkapnya",
-    delay: ""
+    delay: "",
+    url: "https://id.wikipedia.org/wiki/I_Gusti_Ketut_Jelantik"
   },
   {
     id: 5,
@@ -220,7 +256,8 @@ export const PahlawanBelanda = [
     descripion:
       "Ia adalah pahlawan yang berjuang untuk Maluku melawan VOC Belanda, mantan sersan di militer Inggris.",
     button: "Lihat Selengkapnya",
-    delay: "300"
+    delay: "300",
+    url: "https://id.wikipedia.org/wiki/Pattimura"
   }
 ];
 
@@ -233,7 +270,8 @@ export const pahlawanJepang = [
     descripion:
       "Pemimpin Islam yang melakukan perlawanan terhadap pasukan pendudukan Jepang.",
     button: "Lihat Selengkapnya",
-    delay: "1300"
+    delay: "1300",
+    url: "https://id.wikipedia.org/wiki/Zainal_Mustafa"
   },
   {
     id: 2,
@@ -243,7 +281,8 @@ export const pahlawanJepang = [
     descripion:
       "Selama masa pendudukan Jepang di Indonesia, ia menjadi pemimpin pemberontakan milisi PETA di Blitar.",
     button: "Lihat Selengkapnya",
-    delay: "1600"
+    delay: "1600",
+    url: "https://id.wikipedia.org/wiki/Soeprijadi"
   },
   {
     id: 3,
@@ -253,7 +292,8 @@ export const pahlawanJepang = [
     descripion:
       "Memimpin gerilyawan Aceh dalam serangan dan sabotase terhadap pasukan pendudukan Jepang.",
     button: "Lihat Selengkapnya",
-    delay: "1900"
+    delay: "1900",
+    url: "https://id.wikipedia.org/wiki/Teuku_Abdul_Hamid_Azwar"
   },
   {
     id: 4,
@@ -263,7 +303,8 @@ export const pahlawanJepang = [
     descripion:
       "Memimpin rakyat Indramayu dalam melawan kekejaman pasukan pendudukan Jepang.",
     button: "Lihat Selengkapnya",
-    delay: ""
+    delay: "",
+    url: "https://ipsgampang.blogspot.com/2023/04/haji-madriyan-h-madriyas.html"
   }
 ];
 
@@ -276,7 +317,8 @@ export const pahlawanKemerdekaan = [
     descripion:
       "Pemimpin militer yang memimpin perlawanan dalam Pertempuran Surabaya.",
     button: "Lihat Selengkapnya",
-    delay: "1300"
+    delay: "1300",
+    url: "https://id.wikipedia.org/wiki/Sutomo"
   },
   {
     id: 2,
@@ -286,7 +328,8 @@ export const pahlawanKemerdekaan = [
     descripion:
       "Komandan Ketua Tentara Nasional Indonesia pada saat Revolusi Nasional, sosok yang dihormati di Indonesia.",
     button: "Lihat Selengkapnya",
-    delay: "1600"
+    delay: "1600",
+    url: "https://id.wikipedia.org/wiki/Soedirman"
   },
   {
     id: 3,
@@ -296,7 +339,8 @@ export const pahlawanKemerdekaan = [
     descripion:
       "Memimpin militer Bali saat Revolusi Nasional langsung perlawanan bersenjata anti-Belanda di Bali.",
     button: "Lihat Selengkapnya",
-    delay: "1900"
+    delay: "1900",
+    url: "https://id.wikipedia.org/wiki/I_Gusti_Ngurah_Rai"
   },
   {
     id: 4,
@@ -306,7 +350,8 @@ export const pahlawanKemerdekaan = [
     descripion:
       "Pejuang kemerdekaan menentang pemerintah Hindia Belanda di Tanah Karo.",
     button: "Lihat Selengkapnya",
-    delay: ""
+    delay: "",
+    url: "https://id.wikipedia.org/wiki/Jamin_Ginting"
   },
   {
     id: 5,
@@ -316,35 +361,43 @@ export const pahlawanKemerdekaan = [
     descripion:
       "Komodor Angkatan Laut; terbunuh saat konfrontasi dengan Belanda di Nugini Belanda.",
     button: "Lihat Selengkapnya",
-    delay: "300"
+    delay: "300",
+    url: "https://id.wikipedia.org/wiki/Yos_Sudarso"
   }
 ];
 
 export const dataSwiper = [
   {
     id: 1,
-    desc: "Mahasiswa Jurusan Informatika Tahun 2022 PTI Kelas IL - Kelompok 7",
+    add: "Jurusan Informatika Tahun 2022",
+    desc: "Universitas Multimedia Nusantara",
     image: Marcel,
     name: "Marcellus Eugene Kaparang",
     skill: "00000082420"
   },
   {
     id: 2,
-    desc: "Mahasiswa Jurusan Informatika Tahun 2022 PTI Kelas IL - Kelompok 7",
+    add: "Jurusan Informatika Tahun 2022",
+
+    desc: "Universitas Multimedia Nusantara",
     image: Kevin,
     name: "Farrelius Kevin",
     skill: "00000081783"
   },
   {
     id: 3,
-    desc: "Mahasiswa Jurusan Informatika Tahun 2022 PTI Kelas IL - Kelompok 7",
+    add: "Jurusan Informatika Tahun 2022",
+
+    desc: "Universitas Multimedia Nusantara",
     image: Fikri,
     name: "Muhammad Fikri Rouzan Ash Shidik",
     skill: "00000081083"
   },
   {
     id: 4,
-    desc: "Mahasiswa Jurusan Informatika Tahun 2022 PTI Kelas IL - Kelompok 7",
+    add: "Jurusan Informatika Tahun 2022",
+
+    desc: "Universitas Multimedia Nusantara",
     image: Handes,
     name: "Merhandes Gunawan",
     skill: "00000081070"
